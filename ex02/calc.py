3fcf837718143cb08d5c819fad3292df8e423101
aoi <c0a2104428@edu.teu.ac.jp>
2022-06-21 15:31:41 +0900

    課題

--- ex02/calc.py
+++ ex02/calc.py
@@ -2,31 +2,43 @@ import tkinter as tk
 import tkinter.messagebox as tkm
 
 def button_click(event):
-    button = event.widget
-    tkm.showinfo("", f"{num}のボタンがクリックされました")
+    btn = event.widget
+    #tkm.showinfo("", f"{num}のボタンがクリックされました")
+    txt=btn["text"]
+    entry.insert(tk.END, txt)
 
-if __name__ == "__main":
+def click(event):
+    en = entry.get()
+    re = eval(en)
+    entry.delete(0, tk.END)
+    entry.insert(tk.END, str(re))
+
+if __name__ == "__main__":
     root = tk.Tk()
     root.title("練習問題:calc.py")
-    root.geometry("300x500")
+    #root.geometry("300x600")
 
-    entry=tk.Entry(root, justify="right", width=10, font=("Times New Roman", 40))
-    entry.grid(row=0, column=3, columnspan=3)
+    entry=tk.Entry(root, justify="right", width=10, font=("Times New Roman", 30))
+    entry.grid(row=0, column=1, columnspan=4)
+
 
     rnum = 1
     cnum = 1
     numlist = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, "+"]
     for i, num in enumerate(numlist, 1):
         button = tk.Button(root, text=str(num),
-                           width=4, height=2,
                            font=("Times New Roman", 30))
+        button.grid(row=rnum, column=cnum, padx=5, pady=5)
         button.bind("<1>", button_click)
-        button.grid(row=rnum, column=cnum)
         cnum+=1
         if i%3==0:
             rnum+=1
             cnum=0
         #button.pack()
+    
+    btn = tk.Button(root, text="=", font=("Times New Roman", 30))
+    btn.bind("<1>", click)
+    btn.grid(row=rnum, column=cnum, padx=5, pady=5)
 
 
     root.mainloop()
