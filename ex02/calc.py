189dd7268438f20824ccfe4337932bf6c5cd9b89
aoi <c0a2104428@edu.teu.ac.jp>
2022-06-21 16:33:52 +0900

    追加

--- ex02/calc.py
+++ ex02/calc.py
@@ -13,22 +13,27 @@ def click(event):
     entry.delete(0, tk.END)
     entry.insert(tk.END, str(re))
 
+def ac_click(event):
+    entry.delete(0, tk.END)
+
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("練習問題:calc.py")
     #root.geometry("300x600")
 
-    entry=tk.Entry(root, justify="right", width=10, font=("Times New Roman", 30))
+    entry=tk.Entry(root, justify="right", width=10, font=("Times New Roman", 40))
     entry.grid(row=0, column=1, columnspan=4)
 
 
     rnum = 1
-    cnum = 1
-    numlist = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, "+"]
+    cnum = 0
+    numlist = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, "+", "-", "*", "/"]
     for i, num in enumerate(numlist, 1):
         button = tk.Button(root, text=str(num),
-                           font=("Times New Roman", 30))
-        button.grid(row=rnum, column=cnum, padx=5, pady=5)
+                           font=("Times New Roman", 40), 
+                           bg="#58A4B0", fg="white")
+        button.grid(row=rnum, column=cnum, sticky=tk.NSEW)
         button.bind("<1>", button_click)
         cnum+=1
         if i%3==0:
@@ -36,9 +41,13 @@ if __name__ == "__main__":
             cnum=0
         #button.pack()
     
-    btn = tk.Button(root, text="=", font=("Times New Roman", 30))
+    btn = tk.Button(root, text="=", font=("Times New Roman", 40))
     btn.bind("<1>", click)
-    btn.grid(row=rnum, column=cnum, padx=5, pady=5)
+    btn.grid(row=rnum, column=cnum, sticky=tk.NSEW)
+
+    ac = tk.Button(root, text="AC", font=("Times New Roman", 40))
+    ac.bind("<1>", ac_click)
+    ac.grid(row=rnum, column=cnum+1, sticky=tk.NSEW)
 
 
     root.mainloop()
