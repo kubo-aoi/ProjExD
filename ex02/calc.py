5dbfa264e36c2bb515e52fa75984b61e1acc0673
aoi <c0a2104428@edu.teu.ac.jp>
2022-06-21 16:49:40 +0900

    改訂

--- ex02/calc.py
+++ ex02/calc.py
@@ -22,8 +22,9 @@ if __name__ == "__main__":
     root.title("練習問題:calc.py")
     #root.geometry("300x600")
 
-    entry=tk.Entry(root, justify="right", width=10, font=("Times New Roman", 40))
-    entry.grid(row=0, column=1, columnspan=4)
+    entry=tk.Entry(root, justify="right", width=10, font=("Times New Roman", 40),
+                    bg="white")
+    entry.grid(row=0, column=1, columnspan=5)
 
 
     rnum = 1
@@ -31,7 +32,7 @@ if __name__ == "__main__":
     numlist = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, "+", "-", "*", "/"]
     for i, num in enumerate(numlist, 1):
         button = tk.Button(root, text=str(num),
-                           font=("Times New Roman", 40), 
+                           font=("Corben", 40), 
                            bg="#58A4B0", fg="white")
         button.grid(row=rnum, column=cnum, sticky=tk.NSEW)
         button.bind("<1>", button_click)
@@ -41,11 +42,13 @@ if __name__ == "__main__":
             cnum=0
         #button.pack()
     
-    btn = tk.Button(root, text="=", font=("Times New Roman", 40))
+    btn = tk.Button(root, text="=", font=("Time New Roman", 40),
+                    bg="white")
     btn.bind("<1>", click)
     btn.grid(row=rnum, column=cnum, sticky=tk.NSEW)
 
-    ac = tk.Button(root, text="AC", font=("Times New Roman", 40))
+    ac = tk.Button(root, text="AC", font=("Times New Roman", 40),
+                    bg="red")
     ac.bind("<1>", ac_click)
     ac.grid(row=rnum, column=cnum+1, sticky=tk.NSEW)
 
