dbf083bc5cfa5e0efab1e3b1993676a6fbd82852
aoi <c0a2104428@edu.teu.ac.jp>
2022-06-28 15:29:13 +0900

    練習11

--- ex03/maze.py
+++ ex03/maze.py
@@ -11,20 +11,21 @@ def key_down(event):
     #print(f"{key}キーが押されました")
 
 def main_proc():
-    global cx, cy
+    global cx, cy, mx, my
     
     if key == "Up":
-        cy -=20
+        my -=1
     elif key == "Down":
-        cy += 20
+        my += 1
     elif key == "Left":
-        cx -= 20
+        mx -= 1
     elif key == "Right":
-        cx += 20
+        mx += 1
     else:
-        cx += 0
+        mx += 0
     
-        cy += 0
+        my += 0
+    cx, cy = mx*100+50, my*100+50
     canvas.coords("koukaton", cx, cy)
     root.after(100, main_proc)
 
@@ -35,10 +36,11 @@ if __name__ == "__main__":
     canvas = tk.Canvas(root, width=1500, height=900, bg="black")
     canvas.pack()
     maze_bg = mm.make_maze(15, 9)
-    #mm.show_maze(canvas, maze_bg)
+    mm.show_maze(canvas, maze_bg)
 
     koukaton = tk.PhotoImage(file="fig/0.png")
-    cx, cy = 300, 400
+    mx, my = 1, 1
+    cx, cy = mx*100+50, my*100+50
     canvas.create_image(cx, cy, image=koukaton, tag="koukaton")
 
     key = ""
