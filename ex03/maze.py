c4ba6f6ef3fd48b33ffb336f6736d25b0d7358af
aoi <c0a2104428@edu.teu.ac.jp>
2022-06-28 17:09:05 +0900

    4限追加機能実装完了

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 import maze_maker as mm
+import tkinter.messagebox as tkm
+
 
 def key_up(event):
     global key
@@ -14,39 +16,60 @@ def main_proc():
     global cx, cy, mx, my
 
     if key == "Up":
-        my -=1
-        if maze_bg[mx][my] == 1:
-            my += 1
-    elif key == "Down":
-        my += 1
-        if maze_bg[mx][my] == 1:
+        if maze_bg[my-1][mx] == 0:
             my -= 1
+    elif key == "Down":
+        if maze_bg[my+1][mx] == 0:
+            my += 1
     elif key == "Left":
-        mx -= 1
-        if maze_bg[mx][my] == 1:
-            mx += 1
-    elif key == "Right":
-        mx += 1
-        if maze_bg[mx][my] == 1:
+        if maze_bg[my][mx-1] == 0:
             mx -= 1
+    elif key == "Right":
+        if maze_bg[my][mx+1] == 0:
+            mx += 1
     else:
         mx += 0
         my += 0
     
     cx, cy = mx*100+50, my*100+50
     canvas.coords("koukaton", cx, cy)
-    root.after(100, main_proc)
+
+    if mx == 14:
+        if my == 8:
+            game_clear()
+
+    
+def game_clear(self):
+        self.playing=False
+
+        self.canvas.create_text(
+            750,
+            450,
+            font=("", 80),
+            text="ゲームクリア！"
+        )
+
+        self.master.unbind("<KeyPress-Up>")
+        self.master.unbind("<KeyPress-Left>")
+        self.master.unbind("<KeyPress-Right>")
+        self.master.unbind("<KeyPress-Down>")
+
+    
+
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
 
-    canvas = tk.Canvas(root, width=1500, height=900, bg="black")
+    canvas = tk.Canvas(root, width=1500, height=900, bg="white")
     canvas.pack()
     maze_bg = mm.make_maze(15, 9)
+    print(maze_bg)
     mm.show_maze(canvas, maze_bg)
 
-    koukaton = tk.PhotoImage(file="fig/0.png")
+    koukaton = tk.PhotoImage(file="fig/neko.png")
+    koukaton = koukaton.zoom(5)
+    koukaton = koukaton.subsample(32)
     mx, my = 1, 1
     cx, cy = mx*100+50, my*100+50
     canvas.create_image(cx, cy, image=koukaton, tag="koukaton")
@@ -54,6 +77,9 @@ if __name__ == "__main__":
     key = ""
     root.bind("<KeyPress>", key_down)
     root.bind("<KeyRelease>", key_up)
+    if mx == 14:
+        if my == 8:
+            tkm.showwarning(f"GOAL!!!!!Congraturation")
     main_proc()
     
     root.mainloop()
