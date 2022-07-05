19c437e4f0acce8f97d3955a17b547328e43985f
aoi <c0a2104428@edu.teu.ac.jp>
2022-07-05 14:22:49 +0900

    練習3

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -10,9 +10,15 @@ def main():
     bgimg_rct = bgimg_sfc.get_rect()              # Rect
     screen_sfc.blit(bgimg_sfc, bgimg_rct)
     
+    kkimg_sfc = pg.image.load("fig/6.png")
+    kkimg_sfc = pg.transform.rotozoom(kkimg_sfc, 0, 2.0)
+    kkimg_rct = kkimg_sfc.get_rect()
+    kkimg_rct.center = 900, 400
 
     while True:
         screen_sfc.blit(bgimg_sfc, bgimg_rct)
+        screen_sfc.blit(kkimg_sfc, kkimg_rct)
+        
         for event in pg.event.get():              # イベントを繰り返して処理
             if event.type == pg.QUIT:             # ウィンドウの×ボタンをクリックしたら
                 return
