95cd7bc97c328c90a528829f6d95eb9fffb47dcc
aoi <c0a2104428@edu.teu.ac.jp>
2022-07-05 14:53:23 +0900

    練習6

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -17,11 +17,12 @@ def main():
     kkimg_rct.center = 900, 400
 
     #爆弾
-    bomb = pg.Surface((20,20))                              #爆弾用のsurface
-    pg.draw.circle(bomb, (255,0,0),(10,10),10)              #爆弾用surfaceに円を描く。色、中心、半径を指定
-    bomb_rct = bomb.get_rect()                             #爆弾用rect
-    bomb_rct.centerx = randint(0,screen_rct.width)     #爆弾のx座標をランダムに決定
-    bomb_rct.centery = randint(0,screen_rct.height)    #爆弾のy座標をランダムに決定
+    bmimg_sfc = pg.Surface((20,20))  
+    bmimg_sfc.set_colorkey((0, 0, 0))                            #爆弾用のsurface
+    pg.draw.circle(bmimg_sfc, (255,0,0),(10,10),10)              #爆弾用surfaceに円を描く。色、中心、半径を指定
+    bmimg_rct = bmimg_sfc.get_rect()                             #爆弾用rect
+    bmimg_rct.centerx = randint(0,screen_rct.width)     #爆弾のx座標をランダムに決定
+    bmimg_rct.centery = randint(0,screen_rct.height)    #爆弾のy座標をランダムに決定
                                #爆弾用surfaceを画面用surfaceに貼り付ける
     vx, vy = +1, +1
 
@@ -43,6 +44,16 @@ def main():
             kkimg_rct.centerx += 1
         
         screen_sfc.blit(kkimg_sfc, kkimg_rct)
+
+         #　爆弾の移動
+        bmimg_rct.move_ip(vx,vy)                    #爆弾用のrectを移動する
+        screen_sfc.blit(bmimg_sfc, bmimg_rct)                #爆弾の画像を貼り付ける
+        #ret = check_bound(sc_rect, bomb_rect)       #check_bound()関数で画面外にいるかの判定
+        #vx *= ret[0]                                #横方向に画面外なら、横方向速度の符号反転
+        #vy *= ret[1]                                #縦方向に画面外なら、縦方向速度の符号反転
+
+        screen_sfc.blit(bmimg_sfc, bmimg_rct)
+
         
         pg.display.update()
         clock.tick(1000)
